99340351978d70e71f94affde034ba750c9ae32e
rifqi.putro@gmail.com <rifqi.putro@gmail.com>
2026-01-26 21:19:35 +0700

    Update backend untuk admin di supabase dan bikin folder admin-panel

---
+++ admin-panel/js/announcements.js
@@ -0,0 +1,8 @@
+async function getAnnouncements() {
+  const { data, error } = await supabase
+    .from('announcements')
+    .select('*')
+    .order('created_at', { ascending: false })
+
+  if (!error) console.log(data)
+}
